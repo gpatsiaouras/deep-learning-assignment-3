a0b16fa81279b19407e487ce3922d532012ae179
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2020-05-13 23:36:31 +0200

    Added recursive prediction and plots

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import scipy.io
 from keras import Sequential
 from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
-from keras.losses import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
-def construct_dataset(data, window_size):
+def construct_dataset(data):
     input = []
     labels = []
     for idx in range(len(data) - window_size):
@@ -16,11 +18,26 @@ def construct_dataset(data, window_size):
     return np.array(input), np.array(labels)
 
 
-if __name__ == "__main__":
-    # Load data from file
-    dataset = scipy.io.loadmat('data.mat')["X"].astype(float)
+def predict_recursively(predict_size):
+    predictions = np.zeros((predict_size, 1))
 
-    # Scale whole dataset to range [0, 1]
+    # Iterate from the length of the dataset up to plus the predict size
+    for index in range(len(dataset) - predict_size, len(dataset)):
+        # Get the prediction by inputting the last 50 values from the current index
+        current_prediction = model.predict(dataset[np.newaxis, index - window_size:index])
+        predictions[index - len(dataset) + predict_size] = current_prediction
+        dataset[index, 3, 2] = current_prediction
+
+    return predictions
+
+
+if __name__ == "__main__":
+    window_size = 500
+    # Load data from file
+    original_dataset = scipy.io.loadmat('data.mat')["X"].astype(float)
+    dataset = original_dataset.copy()
+
+    # Scale whole dataset to range [0, 1] per feature
     scalers = []
     for feature in range(dataset.shape[2]):
         scaler = MinMaxScaler(feature_range=(0, 1))
@@ -28,15 +45,10 @@ if __name__ == "__main__":
         dataset[..., feature] = scaler.transform(dataset[..., feature])
         scalers.append(scaler)
 
-    # Split dataset
-    train_data = dataset[:-168]
-    test_data = dataset[-168:]
+    train, test = construct_dataset(dataset)
+    x_train, x_test, y_train, y_test = train_test_split(train, test, test_size=168, shuffle=False)
 
-    # Create windows based on the window size
-    x_train, y_train = construct_dataset(train_data, 50)
-    x_test, y_test = construct_dataset(test_data, 50)
-
-    n_steps = 50
+    n_steps = window_size
     n_cities = 4
     n_features = 5
 
@@ -48,11 +60,30 @@ if __name__ == "__main__":
     model.add(Dense(1))
     model.compile(optimizer='adam', loss='mse')
 
-    model.fit(x=x_train, y=y_train, batch_size=1, epochs=10)
+    model.fit(x=x_train, y=y_train, batch_size=50, epochs=20)
 
-    y_pred = model.predict(x=x_test, batch_size=1)
+    # Normal predict
+    y_pred = model.predict(x=x_test, batch_size=50)
+    # Recursive predict
+    y_pred_rec = predict_recursively(predict_size=168)
 
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
+    mse_rec = mean_squared_error(y_test, y_pred_rec)
+    mae_rec = mean_absolute_error(y_test, y_pred_rec)
 
-    print("Mean Squared Error: {}\nMean Absolute Error: {}".format(mse, mae))
+    # Descale
+    temp = np.zeros((len(y_pred), 4, 1))
+    temp[:, 3] = y_pred
+    temp[:, 2] = y_pred_rec
+    descaled_y_pred = scalers[2].inverse_transform(temp[:, :, 0])
+    original_y_test = original_dataset[-168:, 3, 2]
+
+    plt.plot(descaled_y_pred[:, 3], label="Prediction")
+    plt.plot(descaled_y_pred[:, 2], label="Prediction Recursively")
+    plt.plot(original_y_test, label="Original")
+    plt.legend()
+    plt.show()
+
+    print("Normal prediction- Mean Squared Error: {}\nMean Absolute Error: {}\n\n".format(mse, mae))
+    print("Recursive prediction - Mean Squared Error: {}\nMean Absolute Error: {}\n\n".format(mse_rec, mae_rec))
